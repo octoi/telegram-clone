309dbfce8c50f49aac92f8e5479ddef393492a0a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-25 15:25:03 +0530

    app bar

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:telegram/utils/constants.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: appSecondaryColor,
+      appBar: AppBar(
+        backgroundColor: appPrimaryColor,
+        elevation: 0.0,
+        title: Text('Telegram'),
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
+      ),
+    );
+  }
+}
