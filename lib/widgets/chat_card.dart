8fd07d8daa05aa4470264889976514af19028f49
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-25 15:30:08 +0530

    sample chat card

---
+++ lib/widgets/chat_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:telegram/utils/data.dart';
+
+class ChatCard extends StatelessWidget {
+  final ProfileCardContent content;
+
+  const ChatCard({Key? key, required this.content}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
