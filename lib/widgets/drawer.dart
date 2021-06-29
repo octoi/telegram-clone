11139e68d87953af276cff32a11fc490fe363e9d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-29 16:02:09 +0530

    user profile and theme toggler icon

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:telegram/utils/constants.dart';
+import 'package:telegram/utils/data.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({Key? key}) : super(key: key);
@@ -10,12 +11,37 @@ class CustomDrawer extends StatelessWidget {
       child: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        color: appPrimaryColor,
+        color: appSecondaryColor,
         child: SingleChildScrollView(
           physics: BouncingScrollPhysics(),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [],
+            children: [
+              Container(
+                color: appPrimaryColor,
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    SizedBox(height: 40.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: NetworkImage(currentUser.profile),
+                          radius: 30.0,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.wb_sunny),
+                          color: appPrimaryTextColor,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
