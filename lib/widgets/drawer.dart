d287ea079e387fbdce2b83af93a464df1c084226
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-29 16:39:53 +0530

    used drawer tiles and created elements

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:telegram/utils/constants.dart';
 import 'package:telegram/utils/data.dart';
 import 'package:telegram/widgets/drawer_header.dart';
+import 'package:telegram/widgets/drawer_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({Key? key}) : super(key: key);
@@ -19,6 +20,38 @@ class CustomDrawer extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               CustomDrawerHeader(),
+              SizedBox(height: 20.0),
+              DrawerTile(
+                title: "New Group",
+                icon: Icons.people_outline_outlined,
+              ),
+              DrawerTile(
+                title: "Contacts",
+                icon: Icons.person_outline,
+              ),
+              DrawerTile(
+                title: "Calls",
+                icon: Icons.call_outlined,
+              ),
+              DrawerTile(
+                title: "People Nearby",
+                icon: Icons.nature_people_outlined,
+              ),
+              DrawerTile(
+                title: "Saved Messages",
+                icon: Icons.bookmark_outline,
+              ),
+              DrawerTile(
+                title: "Settings",
+                icon: Icons.settings_outlined,
+              ),
+              Divider(),
+              DrawerTile(
+                  title: "Invite Friends", icon: Icons.person_add_outlined),
+              DrawerTile(
+                title: "Telegram Features",
+                icon: Icons.help_outline,
+              ),
             ],
           ),
         ),
